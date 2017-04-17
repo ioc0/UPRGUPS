1d7b052b8970d5a7899697a98ad76a2c1a2267cc
Alexander Kubkin <morttraine@outlook.com>
2017-04-17 12:22:15 +0300

    Программа версия 1

--- UPRGUPS/Form1.cs
+++ UPRGUPS/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace UPRGUPS
 {
@@ -16,5 +17,92 @@ namespace UPRGUPS
         {
             InitializeComponent();
         }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void formLoad(object sender, EventArgs e)
+        {
+            // Initialize the controls and the form.
+            textBox1.Text = Application.StartupPath + "\\sample.xml";
+            textBox1.SetBounds(64, 8, 256, 20);
+            this.Text = "РГУПС Учебная программа.";
+            this.Width = 800;
+            this.Height = 600;
+            treeView1.SetBounds(8, 72, 312, 264);
+            try
+            {
+                // SECTION 1. Create a DOM Document and load the XML data into it.
+                XmlDocument dom = new XmlDocument();
+                dom.Load(textBox1.Text);
+
+                // SECTION 2. Initialize the TreeView control.
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add(new TreeNode(dom.DocumentElement.Name));
+                TreeNode tNode = new TreeNode();
+                tNode = treeView1.Nodes[0];
+
+                // SECTION 3. Populate the TreeView with the DOM nodes.
+                AddNode(dom.DocumentElement, tNode);
+                treeView1.ExpandAll();
+            }
+            catch (XmlException xmlEx)
+            {
+                MessageBox.Show(xmlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xNode;
+            TreeNode tNode;
+            XmlNodeList nodeList;
+            int i;
+
+            // Loop through the XML nodes until the leaf is reached.
+            // Add the nodes to the TreeView during the looping process.
+            if (inXmlNode.HasChildNodes)
+            {
+                nodeList = inXmlNode.ChildNodes;
+                for (i = 0; i <= nodeList.Count - 1; i++)
+                {
+                    xNode = inXmlNode.ChildNodes[i];
+                    inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
+                    tNode = inTreeNode.Nodes[i];
+                    AddNode(xNode, tNode);
+                }
+            }
+            else
+            {
+                // Here you need to pull the data from the XmlNode based on the
+                // type of node, whether attribute values are required, and so forth.
+                inTreeNode.Text = (inXmlNode.OuterXml).Trim();
+            }
+        }
+
+        private void clickOnNode(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            //MessageBox.Show(sender.ToString());
+        }
+
+        private void clickOnItem(object sender, EventArgs e)
+        {
+            MessageBox.Show(sender.ToString());
+        }
     }
+
 }
+    
+
